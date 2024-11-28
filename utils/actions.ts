f1ef975f99bfcbf2906fe3f9acb369ba555eb3ab
emmaGH1 <82253909+emmaGH1@users.noreply.github.com>
2024-11-28 12:33:20 +0100

    feat: Create Profile Action

---
+++ utils/actions.ts
@@ -0,0 +1,41 @@
+'use server'
+
+import db from './db';
+import { auth, clerkClient, currentUser } from '@clerk/nextjs/server';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+import { StateType } from './types';
+import { profileSchema } from './schemas';
+
+export const createProfileAction = async (
+  prevState: StateType,
+  formData: FormData
+) => {
+  try {
+    const user = await currentUser();
+    if (!user) throw new Error('Please login to create a profile');
+
+    const rawData = Object.fromEntries(formData);
+    const validatedFields = profileSchema.parse(rawData);
+
+    await db.profile.create({
+      data: {
+        clerkId: user.id,
+        email: user.emailAddresses[0].emailAddress,
+        profileImage: user.imageUrl ?? '',
+        ...validatedFields,
+      },
+    });
+    const client = await clerkClient()
+    await client.users.updateUserMetadata(user.id, {
+      privateMetadata: {
+        hasProfile: true,
+      },
+    });
+  } catch (error) {
+    return {
+      message: error instanceof Error ? error.message : 'An error occurred',
+    };
+  }
+  redirect('/');
+};
